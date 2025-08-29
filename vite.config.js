047aa83ea8d7886b919e1705977b6353600f3a64
Vyacheslav Shyharenko <shyharenkoslava.dev@email.com>
2025-08-29 18:14:07 +0300

    Created full templete React + Scss

--- vite.config.js
+++ vite.config.js
@@ -1,53 +1,31 @@
 // vite.config.js
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import FullReload from "vite-plugin-full-reload";
-import viteImagemin from "vite-plugin-imagemin";
-import purgeCss from "vite-plugin-purgecss";
 
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   root: "src",
   base: "./",
   publicDir: "../public",
 
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
+      "@": path.resolve("src"),
+    },
+  },
+
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@use "@/scss/abstracts" as *;`,
+      },
     },
   },
 
   build: {
     outDir: "../dist",
     emptyOutDir: true,
-    sourcemap: command === "serve",
-    rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, "src/index.html"),
-      },
-      output: {
-        assetFileNames: "assets/[name]-[hash][extname]",
-        chunkFileNames: "assets/[name]-[hash].js",
-        entryFileNames: "assets/[name]-[hash].js",
-      },
-    },
   },
 
-  plugins: [
-    FullReload(["src/**/*.html", "src/**/*.scss", "src/**/*.js"]),
-    viteImagemin({
-      gifsicle: { optimizationLevel: 7 },
-      optipng: { optimizationLevel: 7 },
-      mozjpeg: { quality: 75 },
-      svgo: {
-        plugins: [
-          { name: "removeViewBox", active: false },
-          { name: "cleanupIDs", active: false },
-        ],
-      },
-    }),
-    purgeCss({
-      content: ["./src/**/*.{html,js,ts}", "./src/index.html"],
-      safelist: [/^observer-delay-/, "visible"],
-    }),
-  ],
-}));
+  plugins: [react()],
+});
